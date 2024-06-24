4996f01d7e8065844d78ce5fb0bb4f6344f8062b
liuchunzhan <a0929900814@gmail.com>
2024-06-24 16:04:35 +0800

    Update app_with_handler.py

--- app_with_handler.py
+++ app_with_handler.py
@@ -64,8 +64,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
+           event.reply_token,
+            ImageSendMessage(
+                    original_content_url = image,
+                    preview_image_url = image)
     )
 
 
